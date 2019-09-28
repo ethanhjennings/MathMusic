8829a2bdbd558c1c053b2d044d9e86e46ce30cc6
Ethan Jennings <ethanhjennings@gmail.com>
2019-09-28 14:18:50 -0700

    Moving WaveGenerator.js to ES6 class and fixing AudioContext needing input issue

--- js/WaveGenerator.js
+++ js/WaveGenerator.js
@@ -1,42 +1,42 @@
-WaveGenerator = function(context,waveFunc) {
-  var that = this;
-  this.context = context;
-  this.node = context.createScriptProcessor(2048, 1, 1);
-  this.node.onaudioprocess = function(e) {
-    waveFunc(that.process(e)) 
-  };
-  this.sampleRate = context.sampleRate;
-  this.sampleFuncs = [];
-  this.current_id = 0;
-}
+class WaveGenerator {
 
-WaveGenerator.prototype.addFunction = function(func,id, params) {
-  this.sampleFuncs.push({func:func,volume:0.0,state:"created",t:0,id:id,params:params});
-}
+  constructor(context, waveCallback) {
+    let that = this;
+    this.context = context;
+    this.node = context.createScriptProcessor(2048, 1, 1);
+    this.node.onaudioprocess = function(e) {
+      waveCallback(that.process(e));
+    };
+    this.sampleRate = context.sampleRate;
+    this.sampleFuncs = [];
+  }
 
-WaveGenerator.prototype.removeFunc = function(id) {
-  for (var j = 0; j < this.sampleFuncs.length; j++) {
-    if (this.sampleFuncs[j].id == id) {
-      this.sampleFuncs[j].state = "deleted";
+  addFunction(func, id, params) {
+    this.sampleFuncs.push({
+      func: func,
+      volume: 0.0,
+      state:"created",
+      t:0,
+      id:id,
+      params:params,
+    });
+  }
+
+  removeFunc(id) {
+    for (let i = 0; i < this.sampleFuncs.length; i++) {
+      if (this.sampleFuncs[i].id == id) {
+        this.sampleFuncs[i].state = "deleted";
+      }
     }
   }
-}
 
-WaveGenerator.prototype.clearFunctions = function() {
-  for (var j = 0; j < this.sampleFuncs.length; j++) {
-    this.sampleFuncs[j].state = "deleted";
-  }
-}
-
-WaveGenerator.prototype.process = function(e) {
-  console.log(this.sampleFuncs.length);
-  try {
-    var data = e.outputBuffer.getChannelData(0);
-    var max = 0.0;
-    for (var i = 0; i < data.length; ++i) {
+  process(audioProcessingEvent) {
+    let data = audioProcessingEvent.outputBuffer.getChannelData(0);
+    let max = 0.0;
+    for (let i = 0; i < data.length; i++) {
       data[i] = 0;
-      for (var j = 0; j < this.sampleFuncs.length; j++) {
-        var s = this.sampleFuncs[j];
+      for (let j = 0; j < this.sampleFuncs.length; j++) {
+        let s = this.sampleFuncs[j];
         data[i] += s.func(s.t,this.sampleRate,s.params.freq)*s.volume;
         s.t++;
       }
@@ -44,13 +44,13 @@ WaveGenerator.prototype.process = function(e) {
         max = Math.abs(data[i]);
       }
       // add decay and fade in functions
-      for (var j = 0; j < this.sampleFuncs.length; j++) {
-        var s = this.sampleFuncs[j];
+      for (let j = 0; j < this.sampleFuncs.length; j++) {
+        let s = this.sampleFuncs[j];
         if (s.state === "created") {
           s.volume = Math.min(s.volume + 0.1, 1.0);
         }
         else if (s.state === "deleted") {
-          s.volume *= 0.9999;
+          s.volume *= 0.999;
         }
       }
 
@@ -62,17 +62,12 @@ WaveGenerator.prototype.process = function(e) {
 
     return data;
   }
-  catch (e) {
-      console.log(e.message);
-      this.sampleFuncs = [];
+  
+  play() {
+    this.node.connect(this.context.destination);
   }
-  return [];
-}
-
-WaveGenerator.prototype.play = function() {
-  this.node.connect(this.context.destination);
-}
-
-WaveGenerator.prototype.pause = function() {
-  this.node.disconnect();
+  
+  pause() {
+    this.node.disconnect();
+  }
 }
